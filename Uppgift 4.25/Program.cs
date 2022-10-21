2385a4040c0f752137b6d2b15cd8472c6aad5cc7
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-10-21 14:44:02 +0200

    Add project files.

---
+++ Uppgift 4.25/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace Uppgift_4._25
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("matte");
+            string räknare = "";
+
+            while (räknare != "3")
+            {
+                Console.WriteLine("Välj ett räknesätt");
+                Console.WriteLine("1. Division");
+                Console.WriteLine("2. Subraktion");
+                Console.WriteLine("3. avsluta programmet");
+                räknare = Console.ReadLine();
+
+                switch (räknare)
+                {
+                    case "1":
+                        Console.WriteLine("Skriv första talet");
+                        int K = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Skriv andra talet");
+                        int k = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"Det här är svart {K / k}");
+                        break;
+                    case "2":
+                        Console.WriteLine("Skriv första talet");
+                        int r = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Skriv andra talet");
+                        int R = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"Det här är svaret {r - R}");
+                        break;
+                    case "3":
+                        Console.WriteLine("Programmet avslutar");
+                        break;
+                    default:
+                        Console.WriteLine("Du har inte valt någon av alternativen");
+                        break;
+
+
+                }
+
+            }
+        }
+
+    }
+}
+
